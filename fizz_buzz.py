86a5c799cd6504ddf761fed503b321791c2ed9fa
ihont <ihont@softserveinc.com>
2023-11-12 14:04:36 +0200

    Updated fizz_buzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,9 +1,16 @@
-for num in range(1, 46):
-    if num % 3 == 0 and num % 5 == 0:
-        print("FizzBuzz")
-    elif num % 3 == 0:
-        print("Fizz")
-    elif num % 5 == 0:
-        print("Buzz")
-    else:
-        print(num)
+random_number = int(input("Please enter a random number"))
+
+
+def fizz_buzz(random_number):
+    for num in range(0, random_number):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+
+fizz_buzz(random_number)
