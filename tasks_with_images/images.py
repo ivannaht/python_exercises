14930196c0ec8f2ca5f664c9a541ca6819e8c6be
ihont <ihont@softserveinc.com>
2024-01-09 23:20:42 +0200

    [main] Added images

--- tasks_with_images/images.py
+++ tasks_with_images/images.py
@@ -1,3 +1,13 @@
 from PIL import Image
 
-mac = Image.open('photo-1.jpg')
+image_1 = Image.open('cat_1.jpeg')
+image_2 = Image.open('dog_1.jpeg')
+image_1.show()
+size_1 = image_1.size
+image_2 = image_2.resize(size_1)
+image_2.show()
+image_2.putalpha(150)
+image_1.paste(image_2, (0,0), image_2)
+image_1.show()
+image_1.close()
+image_2.close()
