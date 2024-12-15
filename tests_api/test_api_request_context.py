c344f56da57c1535b2d7bff9b372b3d129abf843
ivannaht <59062196+ivannaht@users.noreply.github.com>
2024-12-15 20:41:09 +0200

    [main] Updated API tests with playwright

--- tests_api/test_api_request_context.py
+++ tests_api/test_api_request_context.py
@@ -1,10 +1,17 @@
-from playwright.sync_api import Playwright
+import pytest
+from playwright.sync_api import Playwright, APIRequestContext
 
 
-def test_user_data(playwright: Playwright):
+@pytest.fixture
+def api_context(playwright: Playwright) -> APIRequestContext:
     api_context = playwright.request.new_context(
         base_url='https://dummyjson.com'
     )
+    yield api_context
+    api_context.dispose()
+
+
+def test_user_data(api_context: APIRequestContext):
     response = api_context.get('users/3')
 
     user_data = response.json()
@@ -16,3 +23,4 @@ def test_user_data(playwright: Playwright):
     assert user_data['address'] == {'address': '1642 Ninth Street', 'city': 'Washington', 'state': 'Alabama',
                                     'stateCode': 'AL', 'postalCode': '32822', 'coordinates':
                                         {'lat': 45.289366, 'lng': 46.832664}, 'country': 'United States'}
+
